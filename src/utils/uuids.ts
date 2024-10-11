bdc698bae7653f146a4f229c33957ed4cdb7ebff
브레인테크 <e-braintech@braintechco.com>
2024-10-11 17:49:26 +0900

    Bluetooth uuid 데이터 타입 수정

--- src/utils/uuids.ts
+++ src/utils/uuids.ts
@@ -1,3 +1,4 @@
-export const service_UUID: string = '04e60001-3a5b-393f-9a0e-e05ecd42e9ac';
-export const characteristic_UUID: string =
-  '04e60002-3a5b-393f-9a0e-e05ecd42e9ac';
+import {UUID} from 'react-native-ble-plx';
+
+export const service_UUID: UUID = '04e60001-3a5b-393f-9a0e-e05ecd42e9ac';
+export const characteristic_UUID: UUID = '04e60002-3a5b-393f-9a0e-e05ecd42e9ac';
