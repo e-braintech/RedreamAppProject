1838c74aec7f6e9f8e02cfda9fc5d5b200100bd8
브레인테크 <e-braintech@braintechco.com>
2024-10-14 08:55:06 +0900

    Bluetooth Buffer 데이터 수정

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import {AppRegistry} from 'react-native';
 import 'react-native-gesture-handler';
 import App from './App';
 import {name as appName} from './app.json';
-
-global.Buffer = global.Buffer || require('buffer').Buffer;
+import './global.js';
 
 AppRegistry.registerComponent(appName, () => App);
