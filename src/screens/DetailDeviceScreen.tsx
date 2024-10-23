b720c2ddffd9f2369bd8104726855af982a432b4
브레인테크 <e-braintech@braintechco.com>
2024-10-23 17:36:41 +0900

    Bluetooth 높이 설정 드롭다운 컴포넌트 구현중

--- src/screens/DetailDeviceScreen.tsx
+++ src/screens/DetailDeviceScreen.tsx
@@ -1,47 +1,40 @@
 import {RouteProp, useRoute} from '@react-navigation/native';
-import React from 'react';
-import {Pressable, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
 import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
 import {encodeToBase64} from '../common';
+import Dropdown from '../components/Dropdown';
 import {BLEService} from '../services/BLEService';
-import {
-  head_down_down,
-  head_down_up,
-  head_up_down,
-  head_up_up,
-  left_down_down,
-  left_down_up,
-  left_up_down,
-  left_up_up,
-  neck_down_down,
-  neck_down_up,
-  neck_up_down,
-  neck_up_up,
-  nose_left_down_down,
-  nose_left_down_up,
-  nose_left_up_down,
-  nose_left_up_up,
-  nose_right_down_down,
-  nose_right_down_up,
-  nose_right_up_down,
-  nose_right_up_up,
-  right_down_down,
-  right_down_up,
-  right_up_down,
-  right_up_up,
-  shoulder_down_down,
-  shoulder_down_up,
-  shoulder_up_down,
-  shoulder_up_up,
-} from '../utils/actions';
+import {ActionStepType} from '../types/types';
 import {characteristic_UUID, service_UUID} from '../utils/uuids';
 
 type Props = NativeStackScreenProps<ROOT_NAVIGATION, 'DetailDevice'>;
+
+const actionStep: ActionStepType[] = [
+  {step: 1},
+  {step: 2},
+  {step: 3},
+  {step: 4},
+  {step: 5},
+  {step: 6},
+  {step: 7},
+  {step: 8},
+  {step: 9},
+  {step: 10},
+];
 
 const DetailDeviceScreen = ({navigation}: Props) => {
   // View
   const route = useRoute<RouteProp<ROOT_NAVIGATION, 'DetailDevice'>>(); // useRoute로 데이터 접근
   const {deviceId} = route.params; // 전달받은 기기 데이터
+
+  const [selectedStep, setSelectedStep] = useState<number | null>(null);
+
+  // 드롭다운에서 항목 선택 시 호출되는 함수
+  const handleSelectStep = (step: number, index: number) => {
+    setSelectedStep(step);
+    console.log(`Selected Step: ${step}, Index: ${index}`);
+  };
 
   // 데이터를 블루투스 기기로 보내는 함수
   const sendDataToDevice = (data: string) => {
@@ -64,232 +57,130 @@ const DetailDeviceScreen = ({navigation}: Props) => {
 
   // Logic
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         backgroundColor: 'white',
       }}>
-      {/* <Text style={{textAlign: 'center'}}>Device Name: {device.name}</Text>
-      <Text style={{textAlign: 'center'}}>Device ID: {device.id}</Text> */}
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginVertical: 15,
+        }}>
+        <Text style={{fontSize: 18, fontWeight: 'medium', marginRight: 30}}>
+          머리
+        </Text>
+        <Dropdown
+          data={actionStep}
+          selectedStep={selectedStep}
+          onSelect={handleSelectStep}
+        />
+      </View>
 
       <View
         style={{
           flex: 1,
           flexDirection: 'row',
-          justifyContent: 'space-around',
           alignItems: 'center',
+          marginVertical: 15,
         }}>
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(head_up_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(head_up_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>머리 up</Text>
-        </Pressable>
-
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(head_down_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(head_down_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>머리 down</Text>
-        </Pressable>
+        <Text style={{fontSize: 18, fontWeight: 'medium', marginRight: 30}}>
+          목
+        </Text>
+        <Dropdown
+          data={actionStep}
+          selectedStep={selectedStep}
+          onSelect={handleSelectStep}
+        />
       </View>
+
       <View
         style={{
           flex: 1,
           flexDirection: 'row',
-          justifyContent: 'space-around',
           alignItems: 'center',
+          marginVertical: 15,
         }}>
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(neck_up_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(neck_up_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>목 up</Text>
-        </Pressable>
-
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(neck_down_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(neck_down_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>목 down</Text>
-        </Pressable>
+        <Text style={{fontSize: 18, fontWeight: 'medium', marginRight: 30}}>
+          어깨
+        </Text>
+        <Dropdown
+          data={actionStep}
+          selectedStep={selectedStep}
+          onSelect={handleSelectStep}
+        />
       </View>
+
       <View
         style={{
           flex: 1,
           flexDirection: 'row',
-          justifyContent: 'space-around',
           alignItems: 'center',
+          marginVertical: 15,
         }}>
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(shoulder_up_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(shoulder_up_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>어깨 up</Text>
-        </Pressable>
-
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(shoulder_down_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(shoulder_down_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>어깨 down</Text>
-        </Pressable>
+        <Text style={{fontSize: 18, fontWeight: 'medium', marginRight: 30}}>
+          좌측
+        </Text>
+        <Dropdown
+          data={actionStep}
+          selectedStep={selectedStep}
+          onSelect={handleSelectStep}
+        />
       </View>
+
       <View
         style={{
           flex: 1,
           flexDirection: 'row',
-          justifyContent: 'space-around',
           alignItems: 'center',
+          marginVertical: 15,
         }}>
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(left_up_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(left_up_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>좌측 up</Text>
-        </Pressable>
-
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(left_down_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(left_down_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>좌측 down</Text>
-        </Pressable>
+        <Text style={{fontSize: 18, fontWeight: 'medium', marginRight: 30}}>
+          우측
+        </Text>
+        <Dropdown
+          data={actionStep}
+          selectedStep={selectedStep}
+          onSelect={handleSelectStep}
+        />
       </View>
+
       <View
         style={{
           flex: 1,
           flexDirection: 'row',
-          justifyContent: 'space-around',
           alignItems: 'center',
+          marginVertical: 15,
         }}>
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(right_up_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(right_up_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>우측 up</Text>
-        </Pressable>
-
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(right_down_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(right_down_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>우측 down</Text>
-        </Pressable>
+        <Text style={{fontSize: 18, fontWeight: 'medium', marginRight: 30}}>
+          코좌
+        </Text>
+        <Dropdown
+          data={actionStep}
+          selectedStep={selectedStep}
+          onSelect={handleSelectStep}
+        />
       </View>
+
       <View
         style={{
           flex: 1,
           flexDirection: 'row',
-          justifyContent: 'space-around',
           alignItems: 'center',
+          marginVertical: 15,
         }}>
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(nose_left_up_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(nose_left_up_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>코 좌 up</Text>
-        </Pressable>
-
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(nose_left_down_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(nose_left_down_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>코 좌 down</Text>
-        </Pressable>
+        <Text style={{fontSize: 18, fontWeight: 'medium', marginRight: 30}}>
+          코우
+        </Text>
+        <Dropdown
+          data={actionStep}
+          selectedStep={selectedStep}
+          onSelect={handleSelectStep}
+        />
       </View>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-        }}>
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(nose_right_up_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(nose_right_up_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>코 우 up</Text>
-        </Pressable>
-
-        <Pressable
-          style={{
-            backgroundColor: 'blue',
-            padding: 10,
-            marginTop: 20,
-          }}
-          onPressIn={() => sendDataToDevice(nose_right_down_down)} // 버튼을 누를 때 전송
-          onPressOut={() => sendDataToDevice(nose_right_down_up)} // 버튼에서 손을 뗄 때 전송
-        >
-          <Text style={{color: 'white'}}>코 우 down</Text>
-        </Pressable>
-      </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
