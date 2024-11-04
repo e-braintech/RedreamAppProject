bf8929dbd75da9a4d2121aae5ba02fbf311f26d6
브레인테크 <e-braintech@braintechco.com>
2024-11-04 10:12:39 +0900

    Bluetooth 프로토콜 데이터 타입 수정

--- src/utils/common/actions.ts
+++ src/utils/common/actions.ts
@@ -1,83 +1,83 @@
-// 1. 어깨 1단계 요청 값
-export const shoulder_step_1 = 'PA4211%%';
-
-// 1. 어깨 2단계 요청 값
-export const shoulder_step_2 = 'PA4212%%';
-
-// 1. 어깨 3단계 요청 값
-export const shoulder_step_3 = 'PA4213%%';
-
-// 1. 어깨 4단계 요청 값
-export const shoulder_step_4 = 'PA4214%%';
-
-// 1. 어깨 5단계 요청 값
-export const shoulder_step_5 = 'PA4215%%';
-
-// 2. 목 1단계 요청 값
-export const neck_step_1 = 'PA4221%%';
-
-// 2. 목 2단계 요청 값
-export const neck_step_2 = 'PA4222%%';
-
-// 2. 목 3단계 요청 값
-export const neck_step_3 = 'PA4223%%';
-
-// 2. 목 4단계 요청 값
-export const neck_step_4 = 'PA4224%%';
-
-// 2. 목 5단계 요청 값
-export const neck_step_5 = 'PA4225%%';
-
-// 3. 머리 1단계 요청 값
-export const head_step_1 = 'PA4231%%';
-
-// 3. 머리 2단계 요청 값
-export const head_step_2 = 'PA4232%%';
-
-// 3. 머리 3단계 요청 값
-export const head_step_3 = 'PA4233%%';
-
-// 3. 머리 4단계 요청 값
-export const head_step_4 = 'PA4234%%';
-
-// 3. 머리 5단계 요청 값
-export const head_step_5 = 'PA4235%%';
-
-// 4. 머리 우측 1단계 요청 값
-export const right_head_step_1 = 'PA4241%%';
-
-// 4. 머리 우측 2단계 요청 값
-export const right_head_step_2 = 'PA4242%%';
-
-// 4. 머리 우측 3단계 요청 값
-export const right_head_step_3 = 'PA4243%%';
-
-// 4. 머리 우측 4단계 요청 값
-export const right_head_step_4 = 'PA4244%%';
-
-// 4. 머리 우측 5단계 요청 값
-export const right_head_step_5 = 'PA4245%%';
-
-// 5. 머리 좌측 1단계 요청 값
-export const left_head_step_1 = 'PA4251%%';
-
-// 5. 머리 좌측 2단계 요청 값
-export const left_head_step_2 = 'PA4252%%';
-
-// 5. 머리 좌측 3단계 요청 값
-export const left_head_step_3 = 'PA4253%%';
-
-// 5. 머리 좌측 4단계 요청 값
-export const left_head_step_4 = 'PA4254%%';
-
-// 5. 머리 좌측 5단계 요청 값
-export const left_head_step_5 = 'PA4255%%';
-
-// 6. 향기 on 요청 값
-export const smell_on = 'PA326%';
-
-// 6. 향기 off 요청 값
-export const smell_off = 'PA327%%';
-
 // 배터리 상태 표시 요청 값
 export const batteryValue: string = 'PA3710';
+
+// 1. 어깨 1단계 요청 값
+export const shoulder_step_1: string = 'PA421126';
+
+// 1. 어깨 2단계 요청 값
+export const shoulder_step_2: string = 'PA421227';
+
+// 1. 어깨 3단계 요청 값
+export const shoulder_step_3: string = 'PA421328';
+
+// 1. 어깨 4단계 요청 값
+export const shoulder_step_4: string = 'PA421429';
+
+// 1. 어깨 5단계 요청 값
+export const shoulder_step_5: string = 'PA421530';
+
+// 2. 목 1단계 요청 값
+export const neck_step_1: string = 'PA422126';
+
+// 2. 목 2단계 요청 값
+export const neck_step_2: string = 'PA422227';
+
+// 2. 목 3단계 요청 값
+export const neck_step_3: string = 'PA422328';
+
+// 2. 목 4단계 요청 값
+export const neck_step_4: string = 'PA422429';
+
+// 2. 목 5단계 요청 값
+export const neck_step_5: string = 'PA422630';
+
+// 3. 머리 1단계 요청 값
+export const head_step_1: string = 'PA423726';
+
+// 3. 머리 2단계 요청 값
+export const head_step_2: string = 'PA423827';
+
+// 3. 머리 3단계 요청 값
+export const head_step_3: string = 'PA423928';
+
+// 3. 머리 4단계 요청 값
+export const head_step_4: string = 'PA423029';
+
+// 3. 머리 5단계 요청 값
+export const head_step_5: string = 'PA423530';
+
+// 4. 머리 우측 1단계 요청 값
+export const right_head_step_1: string = 'PA424126';
+
+// 4. 머리 우측 2단계 요청 값
+export const right_head_step_2: string = 'PA424227';
+
+// 4. 머리 우측 3단계 요청 값
+export const right_head_step_3: string = 'PA424328';
+
+// 4. 머리 우측 4단계 요청 값
+export const right_head_step_4: string = 'PA424429';
+
+// 4. 머리 우측 5단계 요청 값
+export const right_head_step_5: string = 'PA424530';
+
+// 5. 머리 좌측 1단계 요청 값
+export const left_head_step_1: string = 'PA426125';
+
+// 5. 머리 좌측 2단계 요청 값
+export const left_head_step_2: string = '6A427226';
+
+// 5. 머리 좌측 3단계 요청 값
+export const left_head_step_3: string = '6A428327';
+
+// 5. 머리 좌측 4단계 요청 값
+export const left_head_step_4: string = '7A429428';
+
+// 5. 머리 좌측 5단계 요청 값
+export const left_head_step_5: string = '8A430529';
+
+// 6. 향기 on 요청 값
+export const smell_on: string = 'PA32629';
+
+// 6. 향기 off 요청 값
+export const smell_off: string = 'PA32730';
