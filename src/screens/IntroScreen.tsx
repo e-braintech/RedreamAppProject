4b915fadf3551cd392e36882d487b419ff973763
브레인테크 <e-braintech@braintechco.com>
2024-11-11 09:12:30 +0900

    IntroScreen 구현

--- src/screens/IntroScreen.tsx
+++ src/screens/IntroScreen.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet';
+import React, {useCallback, useMemo, useRef} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
 import ConfirmButton from '../components/ConfirmButton';
+import {useBottomSheetBackHandler} from '../hooks/useBottomSheetBackHandler';
 
 type Props = NativeStackScreenProps<ROOT_NAVIGATION, 'Intro'>;
 
 const IntroScreen = ({navigation}: Props) => {
   // Logic
-  const handleShowLog = () => {
-    console.log('test');
-  };
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const snapPoints = useMemo(() => ['85%'], []);
+
+  const {handleSheetPositionChange} =
+    useBottomSheetBackHandler(bottomSheetModalRef);
+
+  const renderBackdrop = useCallback(
+    (props: any) => <BottomSheetBackdrop {...props} pressBehavior="close" />,
+    [],
+  );
+
+  const handleShowLog = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
 
   // View
   return (
-    <View style={styles.container}>
-      <ConfirmButton
-        title="시작하기"
-        buttonStyle={styles.buttonContainer}
-        textStyle={styles.buttonText}
-        onSubmit={handleShowLog}
-      />
-    </View>
+    <BottomSheetModalProvider>
+      <View style={styles.container}>
+        <ConfirmButton
+          title="시작하기"
+          buttonStyle={styles.buttonContainer}
+          textStyle={styles.buttonText}
+          onSubmit={handleShowLog}
+        />
+
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          enablePanDownToClose={true}
+          backdropComponent={renderBackdrop}
+          onChange={handleSheetPositionChange}>
+          <BottomSheetView style={{flex: 1}}>
+            <View
+              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+              <Text>test</Text>
+            </View>
+          </BottomSheetView>
+        </BottomSheetModal>
+      </View>
+    </BottomSheetModalProvider>
   );
 };
 
